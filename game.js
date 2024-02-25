1dc42e5010438c5b38ae01eb884916adbc3fa5fe
Nefenix <158030014+Nefenix@users.noreply.github.com>
2024-02-25 23:44:21 +0200

    Оригінальна графіка

--- game.js
+++ game.js
@@ -31,7 +31,7 @@ var game = new Phaser.Game(config);
 function preload() { 
     this.load.image('sky', 'assets/sky.png'); 
     this.load.image('ground', 'assets/platform.png'); 
-    this.load.image('star', 'assets/star.png'); 
+    this.load.image('star', 'assets/star.png',); 
     this.load.image('bomb', 'assets/bomb.png'); 
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 }); 
 } 
@@ -129,4 +129,4 @@ function hitBomb(player, bomb) {
 function increaseLevel() { 
     level++; 
     levelText.setText('Level: ' + level); 
-}
+}st
